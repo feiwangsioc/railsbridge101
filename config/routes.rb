8622b5b60d3b6b0102a3be91e0eb59d69ee46e02
fei wang <feiwangsioc@gmail.com>
2019-11-15 04:14:24 +0000

    vote

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :topics do
+   member do
+     post 'upvote'
+     delete 'downvote' 
+     
+   end
+ end
+  root 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
